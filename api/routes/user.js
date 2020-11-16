4d2625b9cb2ea2e953bc63fca615769b604999f2
Anton <alombard0202@gmail.com>
2020-11-16 18:29:09 +0200

    API's

--- api/routes/user.js
+++ api/routes/user.js
@@ -27,7 +27,7 @@ router.post('/signup', (req, res, next) => {
                     user.save().then(result => {
                         console.log(result);
                         res.status(201).json({
-                            message: 'User created'
+                            message: 'User created!'
                         });
                     }).catch(err => {
                         console.log(err);
